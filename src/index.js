8e63f18949a8277ebb2f5910960ffd5fd66c3a92
volkandogan <vo.dogan@gmail.com>
2022-07-23 14:10:12 +0300

    package added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-export function validate(phone) {
+function validate(phone) {
     const regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
     return regex.test(phone)
-  }
- 
+}
+
+module.exports = validate;
